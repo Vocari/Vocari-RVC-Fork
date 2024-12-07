57ccf335ade68fafc7ac0b04b6d37b3d4dd103e6
Neo <neoforevershog@gmail.com>
2024-12-08 06:51:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -132,10 +132,8 @@ with gr.Blocks(title="🔊 Neo RVC WebUI",theme=gr.themes.Soft(primary_hue="gree
         gr.Markdown("# Neo RVC WebUI")
     with gr.Tabs():
         with gr.TabItem("Inference"):
-                with gr.Row():
-                        voice_model = gr.Dropdown(label="Model Voice", choices=sorted(names), value=lambda:sorted(names)[0] if len(sorted(names)) > 0 else '', interactive=True)
-                        file_index2 = gr.Dropdown(label="Change Index",choices=sorted(index_paths),interactive=True,value=sorted(index_paths)[0] if len(sorted(index_paths)) > 0 else '')
-                        
+                voice_model = gr.Dropdown(label="Model Voice", choices=sorted(names), value=lambda:sorted(names)[0] if len(sorted(names)) > 0 else '', interactive=True)
+                file_index2 = gr.Dropdown(label="Change Index",choices=sorted(index_paths),interactive=True,value=sorted(index_paths)[0] if len(sorted(index_paths)) > 0 else '')                        
                                                                  
                 refresh_button = gr.Button("Refresh", variant="primary")
                 spk_item = gr.Slider(
