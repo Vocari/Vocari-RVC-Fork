60431595bbd8deef2afe6b2fd3cd3bee600e418d
Neo <neoforevershog@gmail.com>
2024-12-08 07:05:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -136,7 +136,7 @@ with gr.Blocks(title="🔊 Neo RVC WebUI",theme=gr.themes.Soft(primary_hue="gree
                 with gr.Row():
                     voice_model = gr.Dropdown(label="Model Voice", choices=sorted(names), value=lambda:sorted(names)[0] if len(sorted(names)) > 0 else '', interactive=True)
                     file_index2 = gr.Dropdown(label="Change Index",choices=sorted(index_paths),interactive=True,value=sorted(index_paths)[0] if len(sorted(index_paths)) > 0 else '')                        
-                                                                 
+            with gr.Row():                                               
                 refresh_button = gr.Button("Refresh", variant="primary")
                 spk_item = gr.Slider(
                     minimum=0,
