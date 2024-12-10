05f52e3f33b2f1c040f3709a2e0442ae168ab6c8
Riko <167639579+blane187gt@users.noreply.github.com>
2024-12-10 10:55:49 -0800

    Update app.py

--- app.py
+++ app.py
@@ -520,7 +520,7 @@ with gr.Blocks(
                         value=150,
                         interactive=True,
                     )
-                 with gr.Column():
+                with gr.Column():
                     with gr.Accordion(label="General Settings", open=False):
                         gpus16 = gr.Textbox(
                             label="GPUs separated by -, (e.g. 0-1-2)",
